058a07981f69bb42ca2c9bb7e562add88715cd35
andr68rus <ashtmb@gmail.com>
2018-12-03 14:31:59 +0300

    Revert "config: add qti-telephony-common to build by default"
    This reverts commit 96a1505da633d67dd2ea5eef9c67187fa943ea51.

--- config/common.mk
+++ config/common.mk
@@ -147,10 +147,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.storage_manager.enabled=true
 
-# Telephony-common
-PRODUCT_PACKAGES += \
-    qti-telephony-common
-
 # Disable rescue party
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true
