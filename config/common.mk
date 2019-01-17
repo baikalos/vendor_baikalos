bb0b1ef51cfe182eec9c94efcc3f54302ef1586e
sdv22 <sdv22@live.ru>
2019-01-17 15:07:16 +0300

    Add Google Sans font
    Change-Id: I5c13c49bf4e4f3dcb9f3457348e4b47ab1dbbe2f

--- config/common.mk
+++ config/common.mk
@@ -160,6 +160,17 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/baikalos/overlay/common
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/baikalos/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/baikalos/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/baikalos/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf  \
+    vendor/baikalos/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/baikalos/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/baikalos/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/baikalos/fonts/google-sans.xml
+
 # Boot animation include
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 
