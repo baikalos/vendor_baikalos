0ac17946513dce0ad4b2ed2d44a605cd7ce5a2b0
sdv22 <sdv22@live.ru>
2020-03-18 15:17:01 +0300

    Comment unused props
    Change-Id: If85bffdce72ff9685150ea1dff592b35f6cc4908

--- config/baikalos_props.mk
+++ config/baikalos_props.mk
@@ -11,10 +11,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.build.selinux=1
+ 
+#    ro.com.android.wifi-watchlist=GoogleGuest \
+#    ro.setupwizard.enterprise_mode=1 \
+#    ro.com.android.dateformat=MM-dd-yyyy \
+#    ro.build.selinux=1
 
 # Storage manager
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -25,9 +26,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true
 
 # whitelist packages for location providers not in system
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.services.whitelist.packagelist=com.google.android.gms
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.services.whitelist.packagelist=com.google.android.gms
 
 # Experimental boot_completed offload queue
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.device_config.activity_manager_native_boot.offload_queue_enabled=true
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    persist.device_config.activity_manager_native_boot.offload_queue_enabled=true
