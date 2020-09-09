AICP/vendor_aicp

aicp: Exclude all aicp overlays from RRO
None of this is device specific and therefore needs to be excluded from
RRO overlay packages

Change-Id: I24d882b783a8d7c9e494298e2c26c8ef689a515a

config/common.mk
@@ -18,6 +18,7 @@ include vendor/aicp/config/aosp_audio.mk
 # Google sounds
 include vendor/aicp/google/GoogleAudio.mk
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aicp/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/common
 
 # TWRP

baikalos/vendor_baikalos

config/common.mk
@@ -18,6 +18,7 @@ include vendor/baikalos/config/aosp_audio.mk
 # Google sounds
 include vendor/baikalos/google/GoogleAudio.mk
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/baikalos/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/baikalos/overlay/common
 
 # TWRP