8a95671991a6c6099be1d1828bacee954963a2ca
Sergey Vasiliev <sdv22@live.ru>
2020-12-20 17:31:47 +0300

    Fix romstats
    Change-Id: I05a500969a57913989d8c29b26150401e7bf4a15

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -7,10 +7,10 @@ endif
 # baikalos System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.baikalos.display.version=$(BAIKALOS_VERSION) \
-    ro.baikalos.buildtype=$(BAIKALOS_BUILDTYPE) \
+    ro.baikalos.buildtype=$(BAIKALOS_BUILD_TYPE) \
     ro.baikalos.version.update=$(BAIKALOS_BRANCH)-$(VERSION) \
     ro.modversion=$(BAIKALOS_VERSION) \
-    ro.baikalos.version=$(VERSION)-$(BAIKALOS_BUILDTYPE)
+    ro.baikalos.version=$(VERSION)-$(BAIKALOS_BUILD_TYPE)
 
 # additions for LOS-recovery
 ADDITIONAL_BUILD_PROPERTIES += \
@@ -21,8 +21,8 @@ ADDITIONAL_BUILD_PROPERTIES += \
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.baikalos.branch=$(BAIKALOS_BRANCH) \
     ro.romstats.url=https://stats.baikalos.ru/ \
-    ro.romstats.name=BAIKALOS \
-    ro.romstats.buildtype=$(BAIKALOS_BUILDTYPE) \
+    ro.romstats.name=BaikalOS \
+    ro.romstats.buildtype=$(BAIKALOS_BUILD_TYPE) \
     ro.romstats.version=$(VERSION) \
     ro.romstats.tframe=1 \
     ro.romstats.askfirst=1
