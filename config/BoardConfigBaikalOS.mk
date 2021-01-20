f94421bdb173e440af02e2d734ec6d57db02e352
George Zacharia <george.zcharia@gmail.com>
2021-01-20 08:04:27 +0100

    kernel: Cleanup some of the clang logic
    Only handle the AOSP Clang logic here, other variants will have to depend on the variable available
    
    Change-Id: I3281eca9720a1c498544e6d9b18581ce0b949d4f

--- config/BoardConfigBaikalOS.mk
+++ config/BoardConfigBaikalOS.mk
@@ -12,10 +12,10 @@ ifeq ($(TARGET_USE_MUSICFX),false)
     TARGET_USE_NO_MUSICFX := true
 endif
 
+include vendor/baikalos/config/BoardConfigKernel.mk
 ifeq ($(TARGET_KERNEL_CLANG_VERSION),latest)
 include prebuilts/clang/host/linux-x86_custom/clang_custom.mk
 endif
-include vendor/baikalos/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/baikalos/config/BoardConfigQcom.mk
 endif
