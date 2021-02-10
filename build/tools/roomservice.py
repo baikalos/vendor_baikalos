AICP/vendor_aicp

roomservice: add in support for setting a custom clone-depth for any dependency
Change-Id: I9b5d94cb80b8a276ba5189c2ff0a76910d4c2dea

build/tools/roomservice.py
@@ -206,6 +206,9 @@ def add_to_manifest(repositories, fallback_branch = None):
             project.set('revision', fallback_branch)
         else:
             print("Using default branch for %s" % repo_name)
+        if 'clone-depth' in repo:
+            print("Setting clone-depth to %s for %s" % (repo['clone-depth'], repo_name))
+            project.set('clone-depth', repo['clone-depth'])
 
         lm.append(project)
 

baikalos/vendor_baikalos

build/tools/roomservice.py
@@ -207,6 +207,10 @@ def add_to_manifest(repositories, fallback_branch = None):
         else:
             print("Using default branch for %s" % repo_name)
 
+        if 'clone-depth' in repository:
+            print("Setting clone-depth to %s for %s" % (repository['clone-depth'], repo_name))
+            project.set('clone-depth', repository['clone-depth'])
+
         lm.append(project)
 
     indent(lm, 0)