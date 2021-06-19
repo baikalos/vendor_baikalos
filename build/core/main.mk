2692485aad4f5466213f4937be6effc063aed21e
Sergey Vasiliev <sdv22@live.ru>
2021-06-19 11:19:55 +0300

    Support FP spoofing for GMS, set more keys for snet
    Change-Id: If598e25b0b216578cc95dac2042e2a80e1a11a20

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,12 +1,20 @@
 # Include baikalos versions
 include $(TOPDIR)vendor/baikalos/build/core/main_version.mk
 
-# Override product info for Google Play Services and SafetyNet
+
 ifeq ($(PRODUCT_OVERRIDE_INFO),true)
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.tags=release-keys \
-    ro.build.flavor=$(PRODUCT_NAME)-user \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+
+# Override product info for Google Play Services and SafetyNet
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.tags=release-keys \
+    ro.odm.build.tags=release-keys \
+    ro.product.build.tags=release-keys \
+    ro.system.build.tags=release-keys \
+    ro.system_ext.build.tags=release-keys \
+    ro.vendor.build.tags=release-keys \
+    ro.build.flavor=$(PRODUCT_NAME)-user \
 
 # Description needs special treatment because it contains spaces
 PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
